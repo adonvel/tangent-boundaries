0cb4884d76769c2b6da19f0f37054f7bcbf3d093
adonvel <a.donis.vela@gmail.com>
2025-02-03 17:41:49 +0100

    Included noteboks and files to run the rectangle comparison

---
+++ rectangle.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tangent_boundaries as tb
+from math import pi
+import scipy
+from scipy.sparse import csr_matrix, csc_matrix, linalg as sla
+import sys
+
+
+
+scale_factor = 50
+
+area = scale_factor*scale_factor 
+nbands = 10
+parameters = dict()
+parameters['theta'] = -pi/2
+parameters['mass'] = 1/scale_factor
+
+sides = [x for x in range(scale_factor,5*scale_factor,1) if abs(x*round(scale_factor**2/x)/(scale_factor**2)-1)<0.005]
+#print(len(sides))
+
+#spectrum = np.zeros((len(sides),nbands))
+job = sys.argv[i]
+Lx = sides[job]
+    
+parameters['Nx'] = Lx+2
+parameters['Ny'] = int(area/Lx+2)
+
+ener, states, degenerate_indices = tb.solve_eigenproblem_rectangle(parameters, number_of_bands = nbands, plot_shape=False)
+spectrum = np.sort(ener)
+
+path = '/home/donisvelaa/data1/tangent-boundaries/'
+name = 'rectangles'
+np.save(path+name+'_scalefactor'+str(scale_factor)+'_Lx'+str(Lx), spectrum, allow_pickle=True)
